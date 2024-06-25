d51ceb6ba37453994188532227606ae1714419c5
Nina <nina.dong4764@gmail.com>
2024-06-24 19:12:28 -0700

    added filter

--- frontend/todo/src/App.jsx
+++ frontend/todo/src/App.jsx
@@ -11,7 +11,8 @@ const serverLink = "http://localhost:5000";
 function App() {
   const [todos, setTodos] = useState([{ name: "Loading" }]);
   const [newTodo, setNewTodo] = useState({});
-  const [priority, setPriority] = useState('Low');
+  const [priority, setPriority] = useState("Low");
+  const [priorityFilter, setPriorityFilter] = useState("");
   const getAllTodos = function () {
     axios.get(serverLink + "/api").then((res, req) => {
       setTodos(res.data);
@@ -33,26 +34,28 @@ function App() {
       });
   };
   const handleDeleteTodo = function (_id) {
-    axios.delete(serverLink+"/api/" + _id);
+    axios.delete(serverLink + "/api/" + _id);
     const deletedTodo = todos.filter((todo) => {
       return todo._id != _id;
     });
     setTodos(deletedTodo);
   };
   const handleMarkDone = function (_id) {
-    axios.put(serverLink+'/api/markDone/'+_id, {
-    }).then(()=>{
+    axios.put(serverLink + "/api/markDone/" + _id, {}).then(() => {
       getAllTodos();
-    })
-  }
+    });
+  };
   const handleChangeNewTodo = function (e) {
     setNewTodo(
       e.target.value.slice(0, 1).toUpperCase() + e.target.value.slice(1)
     );
   };
-  const handleChangePriority = function (e) {
-    setPriority(e.target.value)
+  const handleChangePriorityFilter = function(e) {
+    setPriorityFilter(e.target.value);
   }
+  const handleChangePriority = function (e) {
+    setPriority(e.target.value);
+  };
   return (
     <>
       <h1 className="text-center m-4">Todo</h1>
@@ -61,34 +64,57 @@ function App() {
           className="fs-4 form-control"
           onChange={(e) => handleChangeNewTodo(e)}
         ></input>
-        <select onChange={(e)=>handleChangePriority(e)} className="mx-2 w-25 form-control">
+        <select
+          onChange={(e) => handleChangePriority(e)}
+          className="mx-2 w-25 form-control"
+        >
           <option>Low</option>
           <option>Medium</option>
           <option>High</option>
         </select>
         <button
           className="fs-4 px-3 btn btn-success"
-          onClick={(e) => handleAddTodo({ 
-            name: newTodo,
-            isCompleted: false,
-            priority: priority,
-          }, e)}
+          onClick={(e) =>
+            handleAddTodo(
+              {
+                name: newTodo,
+                isCompleted: false,
+                priority: priority,
+              },
+              e
+            )
+          }
         >
           +
         </button>
       </form>
+      <div className="my-4 d-flex align-items-center justify-content-center">
+        <label className="mx-4 fs-3 form-label ">Priority: </label>
+        <select onChange={(e)=>handleChangePriorityFilter(e)}className="fs-4 w-50 form-control">
+          <option value="">None</option>
+          <option value="Low">Low</option>
+          <option value="Medium">Medium</option>
+          <option value="High">High</option>
+
+        </select>
+      </div>
       <ul className="w-75 mx-auto list-group" id="todoList">
         {todos.map((todo, index) => (
-          <div
+          (todo.priority==priorityFilter||priorityFilter=="")&&<div
             key={index}
-            className={`no-bullet todoItems d-flex justify-content-between ${todo.isCompleted&&'text-danger'}`}
+            className={`no-bullet todoItems d-flex justify-content-between ${
+              todo.isCompleted && "text-danger"
+            }`}
           >
             <div className="d-flex">
               <li>{index + 1 + ")" + "\u00A0"}</li>
               <li>{todo.name}</li>
             </div>
             <div>
-              <IoMdCheckmarkCircleOutline onClick={()=>handleMarkDone(todo._id)} color="green" />
+              <IoMdCheckmarkCircleOutline
+                onClick={() => handleMarkDone(todo._id)}
+                color="green"
+              />
               <MdEdit color="blue" />
               <MdOutlineDeleteOutline
                 onClick={() => handleDeleteTodo(todo._id)}
